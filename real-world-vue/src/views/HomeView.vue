9e5a16a1974fb29ee52141630be9bba3a6826842
Silas Jimmy <jimmysilas17@gmail.com>
2023-11-11 06:20:44 +0300

    refactor: Update code to reflect lesson 1 app

--- real-world-vue/src/views/HomeView.vue
+++ real-world-vue/src/views/HomeView.vue
@@ -1,9 +1,54 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import EventCard from "../components/EventCard.vue";
+import { ref } from "vue";
+
+const events = ref([{
+  id: 1,
+  category: 'animal welfare',
+  title: 'cat adoption day',
+  dedscription: 'find your feline friend at this event',
+  location: 'meow town',
+  date: 'january 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'kat laydee'
+},
+{
+  id: 2,
+  category: 'animal welfare',
+  title: 'community gardening',
+  dedscription: 'find your feline friend at this event',
+  location: 'meow town',
+  date: 'january 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'kat laydee'
+},
+{
+  id: 3,
+  category: 'animal welfare',
+  title: 'beach cleanup',
+  dedscription: 'find your feline friend at this event',
+  location: 'meow town',
+  date: 'january 28, 2022',
+  time: '12:00',
+  petsAllowed: true,
+  organizer: 'kat laydee'
+}],
+)
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Events for good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
+  </div>
 </template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
